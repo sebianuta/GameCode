0116365d893f05aee1f2cfcf5bb1b72a4229d17f
sebianuta <156583623+sebianuta@users.noreply.github.com>
2025-06-05 19:14:38 +0300

    Create autoclicker.py

---
+++ autoclicker.py
@@ -0,0 +1,14 @@
+class AutoClicker:
+    def __init__(self):
+        self.clicking = False
+
+    def start_clicking(self):
+        self.clicking = True
+        # Logic to start clicking goes here
+
+    def stop_clicking(self):
+        self.clicking = False
+        # Logic to stop clicking goes here
+
+    def is_clicking(self):
+        return self.clicking  
